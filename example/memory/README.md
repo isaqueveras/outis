946044a6b6f6c81f867f6c48e57f9b231e6e89eb
Isaque Veras <isaqueveras@alu.ufc.br>
2024-03-22 14:04:29 -0300

    feat: example using memory

---
+++ example/memory/README.md
@@ -0,0 +1,26 @@
+# Metrics
+Result of metrics from the sendNotification routine sendNotification
+
+```json
+{
+  "id":"5860c194-4dcc-4c60-b86e-3c9b9dacb9b1",
+  "initialized":"2024-03-22T13:43:43.586312798-03:00",
+  "terminated":"2024-03-22T13:43:43.586371283-03:00",
+  "runtime":0.000058319,
+  "routine":{
+    "id":"b7504beb-1132-4ced-8813-3525523cac1d",
+    "name":"Routine 01",
+    "path":"example/memory/main.go:24"
+  },
+  "metadata":{
+    "notification":{
+      "client_id":234234,
+      "fcm":"3p2okrmionfiun2uni3nfin2i3f",
+      "msg":"Hi, we are notifying you."
+    },
+    "client_id":[
+      234234
+    ]
+  }
+}
+```
---
+++ example/memory/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/isaqueveras/outis"
+	"github.com/isaqueveras/outis/memory"
+)
+
+func main() {
+	watch := outis.Watcher("example/memory", memory.NewOutis())
+
+	go watch.Go(
+		outis.WithID("b7504beb-1132-4ced-8813-3525523cac1d"),
+		outis.WithName("Routine 01"),
+		outis.WithInterval(1),
+		outis.WithHours(12, 13),
+		outis.WithLoadInterval(30),
+		outis.WithRoutine(sendNotification),
+	)
+
+	watch.Wait()
+}
+
+func sendNotification(ctx *outis.Context) {
+	ctx.Info("this is an information message")
+	ctx.Error(errors.New("this is an error message"))
+
+	ctx.Metric("client_ids", []int64{234234})
+	ctx.Metric("notification", outis.Metric{
+		"client_id": 234234,
+		"message":   "Hi, we are notifying you.",
+		"fcm":       "3p2okrmionfiun2uni3nfin2i3f",
+	})
+}
