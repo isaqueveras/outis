b0404fc2ad57dd20d719a826c426139465b9a4d3
Isaque Veras <isaqueveras@alu.ufc.br>
2024-05-22 19:39:25 -0300

    feat: update example

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/isaqueveras/outis"
@@ -20,18 +21,37 @@ func main() {
 }
 
 func example(ctx *outis.Context) {
-	ctx.Metric("uuid", outis.Metric{
+	ctx.Metadata("uuid", outis.Metadata{
 		"success": []uint{23423, 1423, 4322},
 		"failure": []uint{23423, 4546, 3423},
 	})
 
+	totalProblemsResolved := ctx.NewHistogram("problems_resolved")
+	for i := 0; i < 5; i++ {
+		rand.New(rand.NewSource(time.Now().UnixNano()))
+		n := rand.Intn(10 + time.Now().Second()*3)
+		time.Sleep(time.Duration(n) * time.Millisecond)
+		totalProblemsResolved.Add(float64(n))
+	}
+
 	ctx.Debug("Hello 02")
 
-	ctx.Metric("client", outis.Metric{
+	customersNotified := ctx.NewIndicator("customers_notified")
+	customersNotified.Add(13.21)
+
+	totalCanceledProcesses := ctx.NewHistogram("canceled_processes")
+	for i := 0; i < 3; i++ {
+		rand.New(rand.NewSource(time.Now().UnixNano()))
+		n := rand.Intn(10 + time.Now().Second()*5)
+		time.Sleep(time.Duration(n) * time.Millisecond)
+		totalCanceledProcesses.Add(float64(n))
+	}
+
+	ctx.Metadata("client", outis.Metadata{
 		"id":    2134234,
 		"name":  "Antonio Francisco da Silva",
 		"email": "antonio.francisco.silva@email.com",
-		"address": outis.Metric{
+		"address": outis.Metadata{
 			"id":     2345564,
 			"street": "Av. 01",
 			"number": "S/N",
